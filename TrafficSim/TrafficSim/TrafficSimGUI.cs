190b3ee3f1a4ba8ef18dafae417e88355d8d8aea
Sebastian Johansson <johansson.seb@gmail.com>
2009-12-01 14:15:51 -0800

    Graphics working.

--- TrafficSim/TrafficSim/TrafficSimGUI.cs
+++ TrafficSim/TrafficSim/TrafficSimGUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -33,9 +34,20 @@ namespace TrafficSim
             g.DrawEllipse(new Pen(Color.Black, wR), wR / 2, wR/2, wE, wE);
 
             // CARS
-            float wC = (float)Car.CAR_WIDTH * scale / 2;
-            float lC = (float)Car.CAR_LENGTH * scale / 2;
-            //Point[] carModel = {new Point(
+            int wCu = (int)Math.Round(wR/2 + Car.CAR_WIDTH * scale / 2);
+            int wCl = (int)Math.Round(wR/2 - Car.CAR_WIDTH * scale / 2);
+            int lCl = (int)Math.Round(wE/2 + wR/2 - Car.CAR_LENGTH * scale / 2);
+            int lCr = (int)Math.Round(wE/2 + wR/2 + Car.CAR_LENGTH * scale / 2);
+
+            foreach (Vehicle v in road.Vehicles)
+            {
+                Point[] carModel = { new Point(lCl, wCu), new Point(lCr, wCu), new Point(lCr, wCl), new Point(lCl, wCl) };
+                Matrix transMatrix = new Matrix();
+                transMatrix.RotateAt((float)v.Position.ToDegrees(), new PointF((wE + wR) / 2, (wE + wR) / 2));
+                transMatrix.TransformPoints(carModel);
+
+                g.FillPolygon(new SolidBrush(Color.Red), carModel);
+            }
 
         }
         #endregion
