fb8c1b8e43933c6e01deabd0b2448ac94211dfd5
Sebastian Johansson <johansson.seb@gmail.com>
2009-12-02 19:08:55 +0100

    Skall l?gga till ett diagram...

--- TrafficSim/TrafficSim/Car.cs
+++ TrafficSim/TrafficSim/Car.cs
@@ -9,7 +9,7 @@ namespace TrafficSim
         public static readonly double CAR_LENGTH = 5.0;
         public static readonly double CAR_WIDTH = 1.8;
 
-        public Car(Road road) : base(road)
+        public Car(Road road, double maxAcc, double maxBrake) : base(road, maxAcc, maxBrake)
         {
             this.length = CAR_LENGTH;
             this.width = CAR_WIDTH;
