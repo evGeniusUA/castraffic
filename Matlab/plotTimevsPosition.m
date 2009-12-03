292e0f170ba3130b8e8d0bf6979007e946aff273
Simon Lindkvist <simon.lindkvistl@gmail.com>
2009-12-03 18:16:21 +0100

    Uppdateringar av modellen

--- Matlab/plotTimevsPosition.m
+++ Matlab/plotTimevsPosition.m
@@ -5,6 +5,11 @@ plot(Positions(:,:),Time,'.','MarkerSize',1);
 figure(2);
 hold off;
 plot(Time,Velocity(:,:),'.','MarkerSize',1);
+hold on;
+%for i = 1:50
+%    figure(i+4);
+%    plot(Time,Velocity(:,i),'-','MarkerSize',1);
+%end
 
     
 figure(3);
@@ -13,7 +18,7 @@ g = Positions';
 [B,IX] = sort(g);
 f = Velocity';
 
-for j = 1:10:size(f,2);
+for j = 1:1:1 %size(f,2);
     C(:,j) = f(IX(:,j),j); 
     plot(B(:,j),C(:,j),'MarkerSize',8);
     axis([0 800 0 50]);
--- TrafficSim/TrafficSim/Vehicle.cs
+++ TrafficSim/TrafficSim/Vehicle.cs
@@ -7,9 +7,25 @@ namespace TrafficSim
     public class Vehicle
     {
         #region Properties
-        public Radian Position { get; set; } //For circular road in radians
+
+        //For circular road in radians
+        protected Radian newPosition;
+        protected Radian position;
+        public Radian Position
+        {
+            get
+            {
+                return this.position;
+            }
+            set
+            {
+                this.position = value;
+            }
+        }
+
         private Queue<double> accelerationHistory; //FIFO-buffer with acceleration
 
+        protected double newVelocity;
         protected double velocity;
         public double Velocity
         {
@@ -119,9 +135,16 @@ namespace TrafficSim
             this.maxBrake = maxBrake;
             this.driver = drv;
             this.velocity = 5;
+            this.newVelocity = this.velocity;
             this.accelerationHistory = new Queue<double>();
-            for(int i = 0; i <1/this.Road.TimeStepSize; i++)
+            for(int i = 0; i <0.6/this.Road.TimeStepSize; i++)
                 accelerationHistory.Enqueue(0);
+        }
+
+        public void UpdatePosAndVel()
+        {
+            this.position = this.newPosition;
+            this.velocity = this.newVelocity;
         }
 
         public double DistanceToNextVehicle()
@@ -132,7 +155,7 @@ namespace TrafficSim
 
         private void MoveToNewPos(double distanceToMove)
         {
-            this.Position = this.Position + Radian.FromDistance(distanceToMove, this.Road.RoadRadius);
+            this.newPosition = this.position + Radian.FromDistance(distanceToMove, this.Road.RoadRadius);
         }
 
         public void Iterate(double timeStepSize)
@@ -141,7 +164,7 @@ namespace TrafficSim
             double delta = 4;   //Constant between 1-5, "behaviour of driver"
             double a = this.MaxAcceleration; //Maximum acceleration
             double b = this.MaxBrake; //Maximum brake
-            double s0 = 2; //Minimum gap
+            double s0 = 1.5; //Minimum gap
             double T = 0.5 + this.Driver; //Time headway
             double deltaV = this.Velocity - this.NextVehicle.Velocity; // +(this.Velocity - this.NextVehicle.Velocity) * 2 * (this.Driver - 0.5); //Difference in velocity
                     
@@ -151,9 +174,9 @@ namespace TrafficSim
             accelerationHistory.Enqueue(a * (1 - Math.Pow(this.Velocity / v0, delta) - Math.Pow(sStar / sa, 2))); //Update acceleration
 
             this.acceleration = accelerationHistory.Dequeue();
-            this.velocity += this.Acceleration * timeStepSize; //Update velocity
-            this.velocity = Math.Max(this.Velocity, 0);
-            this.MoveToNewPos(this.Velocity * timeStepSize+ (1/2) * this.Acceleration * Math.Pow(timeStepSize,2)); //Update movement
+            this.newVelocity += this.Acceleration * timeStepSize; //Update velocity
+            this.newVelocity = Math.Max(this.newVelocity, 0);
+            this.MoveToNewPos(this.newVelocity * timeStepSize+ (1/2) * this.Acceleration * Math.Pow(timeStepSize,2)); //Update movement
         }
     }
 }
